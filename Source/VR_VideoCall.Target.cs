e2e4c41b09e499cd4191e36c19a574474aaa139f
AbolfazlMoghanlu <Abolfazlmoghanlu@gmail.com>
2023-10-20 17:55:04 +0330

    Add agora example source files

--- Source/VR_VideoCall.Target.cs
+++ Source/VR_VideoCall.Target.cs
@@ -10,6 +10,21 @@ public class VR_VideoCallTarget : TargetRules
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 
+		if (Target.Platform == UnrealTargetPlatform.IOS)
+		{
+			bOverrideBuildEnvironment = true;
+			GlobalDefinitions.Add("FORCE_ANSI_ALLOCATOR=1");
+		}
+
 		ExtraModuleNames.AddRange( new string[] { "VR_VideoCall" } );
+		
+		 if (Target.Platform == UnrealTargetPlatform.IOS || Target.Platform == UnrealTargetPlatform.Mac)
+        {
+            bOverrideBuildEnvironment = true;
+            // -Wno-unused-but-set-variable£º fix error "variable 'layerNames' set but not used"
+            //  -Wno-gcc-compat: gcc does not allow an atrribute in this position on a function declaration
+            // -Wno-reorder-ctor: fix error "field 'eventHandler' will be initialized after field 'mccUid'"
+            AdditionalCompilerArguments = "-Wno-unused-but-set-variable -Wno-gcc-compat -Wno-reorder-ctor";
+        }
 	}
 }
